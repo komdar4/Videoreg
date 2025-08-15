5c84c5097065d6b8d184b46a5456243b037a7ee1
komdar4 <komdar4@gmail.com>
2025-08-15 22:09:59 +0300

    Добавлен запуск клавиатуры, обзорщик папок, ini файл, about

--- main.cpp
+++ main.cpp
@@ -15,20 +15,52 @@
 #include <QRegularExpressionValidator>
 #include <QScreen>
 #include <QMediaMetaData>
+#include <QSettings>
 #include <QTimer>
 #include <iostream>
 
-#define URL_CAMERA                  "rtsp://192.168.0.237"
-#define BASE_PATH                   "video"
 #define NUMBER_FILES_FOR_PRESON     99
+#define ABOUT   "Программно-аппаратный комплекс CamRecorder, версия 1.0.0"
+
+class MyLineEdit : public QLineEdit
+{
+    Q_OBJECT
+public:
+    explicit MyLineEdit(QWidget *parent = nullptr) : QLineEdit(parent)
+    {
+        keyboard_process = new QProcess(this);
+    }
+    virtual ~MyLineEdit()
+    {
+        if(keyboard_process != nullptr)
+            delete keyboard_process;
+    }
+protected:
+    void mousePressEvent([[maybe_unused]]QMouseEvent *event) override
+    {
+        if(keyboard_process->state() == QProcess::Running)
+            return;
+        keyboard_process->start("osk", {});
+        if (!keyboard_process->waitForStarted()) {
+            QMessageBox::critical(this, "Ошибка", "Не удалось открыть клавиатуру");
+            return;
+        }
+    }
+private:
+    QProcess* keyboard_process = nullptr;
+};
 
 class RTSPRecorder : public QMainWindow {
     Q_OBJECT
 public:
     RTSPRecorder(QWidget *parent = nullptr) : QMainWindow(parent) {
-        setWindowTitle("Video-reg");
+        setWindowTitle("CamRecorder");
 
-        person_last_name = new QLineEdit();
+        QSettings settings("CamRecorder.ini", QSettings::IniFormat);
+        rtspUrl = settings.value("Config/URLCamera", "").toString();
+        base_path = settings.value("Config/PathSavingVideos", "").toString();
+
+        person_last_name = new MyLineEdit();
         person_last_name->setFixedSize(500, 50);
         person_last_name->setMaxLength(30);
         QFont last_name_font = person_last_name->font();
@@ -45,13 +77,41 @@ public:
         recordButton->setFont(*record_button_text);
         recordButton->setEnabled(false);
 
+        about_button = new QPushButton();
+        about_button->setText("i");
+        QFont about_font = about_button->font();
+        about_font.setPointSize(20);
+        about_button->setFont(about_font);
+        about_button->setFixedSize(50, 50);
+
         videoWidget = new QVideoWidget();
         int screen_height = QGuiApplication::primaryScreen()->geometry().height();
-        videoWidget->setMinimumHeight(screen_height - button_hight - 90);
+        videoWidget->setMinimumHeight(screen_height - button_hight - settings.value("Config/VerticalCorrection", 90).toInt());
 
         // Медиаплеер для вывода RTSP
         mediaPlayer = new QMediaPlayer(this);
         mediaPlayer->setVideoOutput(videoWidget);
+
+        dialog = new QFileDialog(this);
+        dialog->setFileMode(QFileDialog::Directory);
+        dialog->setOption(QFileDialog::ShowDirsOnly, true);
+        dialog->setWindowTitle(tr("Выберите папку"));
+
+        path_selection = new QPushButton();
+        path_selection->setFixedSize(50, 50);
+        QIcon icon("images/path.png");
+        path_selection->setIcon(icon);
+        path_selection->setIconSize(QSize(24, 24));
+
+        about_dialog = new QDialog();
+        about_dialog->setWindowTitle("Информация");
+        about_dialog->setMinimumSize(400, 200);
+        layout_about_text = new QVBoxLayout();
+        text_about = new QLabel();
+        text_about->setText(ABOUT);
+        about_dialog->setLayout(layout_about_text);
+        layout_about_text->addWidget(text_about);
+        layout_about_text->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
 
         QVBoxLayout *mainLayout = new QVBoxLayout();
         QHBoxLayout *controlLayout = new QHBoxLayout();
@@ -63,7 +123,9 @@ public:
         controlLayout->setAlignment(Qt::AlignBottom | Qt::AlignLeft);
         controlLayout->addWidget(recordButton);
         controlLayout->addWidget(person_last_name);
+        controlLayout->addWidget(path_selection);
         controlLayout->addStretch(1);
+        controlLayout->addWidget(about_button);
 
         timer = new QTimer(this);
         connect(timer, &QTimer::timeout, this, &RTSPRecorder::onTimeout);
@@ -71,6 +133,9 @@ public:
 
         // Обработчики кнопок
         connect(recordButton, &QPushButton::clicked, this, &RTSPRecorder::Recording);
+        connect(recordButton, &QPushButton::clicked, this, &RTSPRecorder::ChangeStateRecordButton);
+        connect(path_selection, &QPushButton::clicked, this, &RTSPRecorder::PathSelect);
+        connect(about_button,  &QPushButton::clicked, this, &RTSPRecorder::PrintAbout);
         connect(mediaPlayer, &QMediaPlayer::errorOccurred, this, &RTSPRecorder::handleMediaError);
         connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, &RTSPRecorder::CameraFound);
         ffmpegProcess = new QProcess(this);
@@ -88,7 +153,9 @@ private slots:
             QString outputFile = person_last_name->text().trimmed();
             if ( outputFile.isEmpty())
                 return;
-            QString file_directory = QString(BASE_PATH) + QString("/") + QDate::currentDate().toString("yyyy-MM-dd");
+            if(base_path.endsWith('/'))
+                base_path.chop(1);
+            QString file_directory = base_path + "/" + QString("video") + QString("/") + QDate::currentDate().toString("yyyy-MM-dd");
             if(!QDir(file_directory).exists())
                 QDir().mkpath(file_directory);
             QFileInfoList fileList = QDir(file_directory).entryInfoList(QStringList() << ( outputFile + "*.mp4" ), QDir::Files | QDir::Readable, QDir::Name);
@@ -120,17 +187,24 @@ private slots:
         }
         if(ffmpegProcess->state() == QProcess::Running)
         {
-            ffmpegProcess->write("q");
+            if(!was_error)
+                ffmpegProcess->write("q");
+            else
+                ffmpegProcess->kill();
             ffmpegProcess->waitForFinished();
         }
         recordButton->setText("Записать");
         record_state = true;
     }
 
-    void onFfmpegFinished(int exitCode, QProcess::ExitStatus exitStatus) {
-        if (exitStatus == QProcess::NormalExit && exitCode == 0) {
+    void onFfmpegFinished(int exitCode, QProcess::ExitStatus exitStatus)
+    {
+        if (exitStatus == QProcess::NormalExit && exitCode == 0)
+        {
             QMessageBox::information(this, "Готово", "Запись завершена успешно!");
-        } else {
+        }
+        else
+        {
             QMessageBox::warning(this, "Ошибка", 
                 QString("FFmpeg завершился с ошибкой (код: %1)").arg(exitCode));
         }
@@ -140,18 +214,22 @@ private slots:
         if(was_error)
             return;
         was_error = true;
-        recordButton->setEnabled(false);
+        if(!record_state)
+            Recording(); // Если запись идёт, то при потере связи переводим кнопку, файл финализируется сам ffmpeg-ом
+        ChangeStateRecordButton();
         QMessageBox::critical(this, "Ошибка", "Нет сигнала");
         timer->start();
     }
 
     void CameraFound(QMediaPlayer::MediaStatus status)
     {
-        if( status != QMediaPlayer::MediaStatus::BufferingMedia)
+        if(status == QMediaPlayer::MediaStatus::BufferingMedia)
+            connect(person_last_name, &QLineEdit::textChanged, this, &RTSPRecorder::LastNameChanged);
+        if(status != QMediaPlayer::MediaStatus::BufferingMedia || !was_error)
             return;
         timer->stop();
-        recordButton->setEnabled(true);
         was_error = false;
+        LastNameChanged();
     }
 
     void onTimeout()
@@ -162,16 +240,57 @@ private slots:
         mediaPlayer->play();
     }
 
+    void LastNameChanged()
+    {
+        last_name_is_empty = person_last_name->text().isEmpty() ? true : false;
+        ChangeStateRecordButton();
+    }
+
+    void PathSelect()
+    {
+        if(dialog->isActiveWindow())
+            return;
+        if(dialog->exec() == QDialog::Accepted)
+        {
+            base_path = dialog->selectedFiles().first();
+            QSettings settings("CamRecorder.ini", QSettings::IniFormat);
+            settings.setValue("Config/PathSavingVideos", base_path);
+        }
+    }
+
+    void PrintAbout()
+    {
+        if(about_dialog->isActiveWindow())
+            return;
+        about_dialog->exec();
+    }
+
 private:
-    QLineEdit *person_last_name;
+    MyLineEdit *person_last_name;
     QPushButton *recordButton;
+    QPushButton *path_selection;
+    QPushButton *about_button;
     QProcess *ffmpegProcess;
     QMediaPlayer *mediaPlayer;
     QVideoWidget *videoWidget;
-    QString rtspUrl = URL_CAMERA;
+    QString rtspUrl;
+    QString base_path;
     QTimer* timer;
+    QFileDialog *dialog;
+    QDialog *about_dialog;
+    QVBoxLayout *layout_about_text;
+    QLabel *text_about;
     bool record_state = true;
     bool was_error = false;
+    bool last_name_is_empty = true;
+
+    void ChangeStateRecordButton()
+    {
+        if((was_error || last_name_is_empty) && record_state)
+            recordButton->setEnabled(false);
+        else
+            recordButton->setEnabled(true);
+    }
 };
 
 int main(int argc, char *argv[]) {
--- main.pro
+++ main.pro
@@ -1,6 +1,6 @@
 QT += widgets multimedia multimediawidgets
 QT += core gui
 CONFIG += c++17
-TARGET = RTSPRecorder
+TARGET = CamRecorder
 SOURCES += main.cpp
 CONFIG += qml debug
